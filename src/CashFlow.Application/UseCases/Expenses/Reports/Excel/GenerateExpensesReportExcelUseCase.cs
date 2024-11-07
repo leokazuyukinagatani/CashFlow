85eaeaf444de75382b0c48f92a86cf13191fa15b
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2024-11-07 18:37:59 -0300

    feat: gerando excel

---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -0,0 +1,47 @@
+﻿using CashFlow.Domain.Reports;
+using ClosedXML.Excel;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Excel;
+
+public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
+{
+    public async Task<byte[]> Execute(DateOnly month)
+    {
+        var workbook =  new XLWorkbook();
+
+        workbook.Author = "Leo Kazuyuki Nagatani";
+        workbook.Style.Font.FontSize = 12;
+        workbook.Style.Font.FontName = "Times New Roman";
+
+
+        var worksheet = workbook.Worksheets.Add($"Reports of {month:Y}");
+
+        InsertHeader( worksheet );
+
+        var file = new MemoryStream();
+
+        workbook.SaveAs(file);
+
+        return file.ToArray();
+    }
+
+    private void InsertHeader(IXLWorksheet worksheet)
+    {
+        worksheet.Cell("A1").Value = ResourceReportGenerationMessage.TITLE;
+        worksheet.Cell("B1").Value = ResourceReportGenerationMessage.DATE;
+        worksheet.Cell("C1").Value = ResourceReportGenerationMessage.PAYMENT_TYPE;
+        worksheet.Cell("D1").Value = ResourceReportGenerationMessage.AMOUNT;
+        worksheet.Cell("E1").Value = ResourceReportGenerationMessage.DESCRIPTION;
+
+        worksheet.Cells("A1:E1").Style.Font.Bold = true;
+
+        worksheet.Cells("A1:E1").Style.Fill.BackgroundColor = XLColor.FromHtml("#F5C2B6");
+
+        worksheet.Cell("A1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        worksheet.Cell("B1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        worksheet.Cell("C1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        worksheet.Cell("E1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+
+        worksheet.Cell("D1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Right);
+    }
+}
