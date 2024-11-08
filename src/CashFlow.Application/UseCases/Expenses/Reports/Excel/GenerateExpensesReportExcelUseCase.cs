8a30a1e084f53734e1676de3d700c066d1f54363
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2024-11-08 10:20:12 -0300

    feat: adicionando relatorio excel

--- src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -1,13 +1,28 @@
-﻿using CashFlow.Domain.Reports;
+﻿using CashFlow.Domain.Enums;
+using CashFlow.Domain.Reports;
+using CashFlow.Domain.Repositories.Expenses;
 using ClosedXML.Excel;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Excel;
 
 public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
 {
+    private const string CURRENCY_SYMBOL = "€";
+    private readonly IExpensesReadOnlyRepository _repository;
+
+    public GenerateExpensesReportExcelUseCase(IExpensesReadOnlyRepository repository)
+    {
+        _repository = repository;
+    }
     public async Task<byte[]> Execute(DateOnly month)
     {
-        var workbook =  new XLWorkbook();
+        var expenses = await _repository.FilterByMonth(month);
+
+        if (expenses.Count == 0) {
+            return [];
+        }
+
+        using var workbook =  new XLWorkbook();
 
         workbook.Author = "Leo Kazuyuki Nagatani";
         workbook.Style.Font.FontSize = 12;
@@ -18,11 +33,42 @@ public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUs
 
         InsertHeader( worksheet );
 
+        var raw = 2;
+        foreach (var expense in expenses) {
+            worksheet.Cell($"A{raw}").Value = expense.Title;
+            worksheet.Cell($"B{raw}").Value = expense.Date;
+            worksheet.Cell($"C{raw}").Value = ConvertPaymentType(expense.PaymentType);
+
+            worksheet.Cell($"D{raw}").Value = expense.Amount;
+            worksheet.Cell($"D{raw}").Style.NumberFormat.Format = $"-{CURRENCY_SYMBOL} #,##0.00";
+
+            worksheet.Cell($"E{raw}").Value = expense.Description;
+
+
+            raw++;
+        }
+
+        worksheet.Columns().AdjustToContents();
+
         var file = new MemoryStream();
 
         workbook.SaveAs(file);
 
         return file.ToArray();
+    }
+
+    
+
+    private static string ConvertPaymentType(PaymentType  payment)
+    {
+        return payment switch
+        {
+            PaymentType.Cash => ResourceReportGenerationMessage.CASH,
+            PaymentType.CreditCard => ResourceReportGenerationMessage.CREDIT_CARD,
+            PaymentType.DebitCard => ResourceReportGenerationMessage.DEBIT_CARD,
+            PaymentType.EletronicTransfer => ResourceReportGenerationMessage.ELETRONIC_TRANSFER,
+            _ => string.Empty
+        };
     }
 
     private void InsertHeader(IXLWorksheet worksheet)
