83e88dbbf2b1fd78bd0c036c2c04114e50fa62f6
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2024-11-08 16:23:23 -0300

    feat: relatorio pdf

---
+++ src/CashFlow.Domain/Extensions/PaymentTypeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using CashFlow.Domain.Enums;
+using CashFlow.Domain.Reports;
+
+namespace CashFlow.Domain.Extensions;
+
+public static class PaymentTypeExtensions
+{
+    public static string PaymentTypeToString(this PaymentType paymentType)
+    {
+        return paymentType switch
+        {
+            PaymentType.Cash => ResourceReportGenerationMessage.CASH,
+            PaymentType.CreditCard => ResourceReportGenerationMessage.CREDIT_CARD,
+            PaymentType.DebitCard => ResourceReportGenerationMessage.DEBIT_CARD,
+            PaymentType.EletronicTransfer => ResourceReportGenerationMessage.ELETRONIC_TRANSFER,
+            _ => string.Empty
+        };
+
+    }
+}
