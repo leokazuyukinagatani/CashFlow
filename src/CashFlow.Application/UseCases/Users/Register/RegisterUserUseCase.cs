f9413c82462e72872394bb3b5d5b80143d49cf9d
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2024-11-11 20:12:46 -0300

    feat: gerando token e devolvendo

--- src/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
+++ src/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -4,6 +4,7 @@ using CashFlow.Communication.Responses;
 using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Users;
 using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Domain.Security.Tokens;
 using CashFlow.Exception;
 using CashFlow.Exception.ExceptionsBase;
 using FluentValidation.Results;
@@ -16,18 +17,21 @@ public class RegisterUserUseCase : IRegisterUserUseCase
     private readonly IPassWordEncripter _passWordEncripter;
     private readonly IUsersReadOnlyRepository _usersReadOnlyRepository;
     private readonly IUsersWriteOnlyRepository _usersWriteOnlyRepository;
+    private readonly IAccessTokenGenerator _tokenGenerator;
     private readonly IUnitOfWork _unitOfWork;
 
     public RegisterUserUseCase(
         IMapper mapper,
         IUnitOfWork unitOfWork,
         IPassWordEncripter passWordEncripter, 
+        IAccessTokenGenerator tokenGenerator,
         IUsersReadOnlyRepository usersReadOnlyRepository,
         IUsersWriteOnlyRepository usersWriteOnlyRepository)
     {
         _mapper = mapper;
         _passWordEncripter = passWordEncripter;
         _usersReadOnlyRepository = usersReadOnlyRepository;
+        _tokenGenerator = tokenGenerator;
         _usersWriteOnlyRepository = usersWriteOnlyRepository;
         _unitOfWork = unitOfWork;
     }
@@ -47,6 +51,7 @@ public class RegisterUserUseCase : IRegisterUserUseCase
         return new ResponseRegisteredUserJson
         {
             Name = user.Name,
+            Token = _tokenGenerator.Generate(user)
         };
     }
 
--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -2,8 +2,10 @@
 using CashFlow.Domain.Repositories.Expenses;
 using CashFlow.Domain.Repositories.Users;
 using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Domain.Security.Tokens;
 using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.DataAccess.Repositories;
+using CashFlow.Infrastructure.Security.Tokens;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,8 +18,16 @@ public static class DependencyInjectionExtension
     {
         AddDbContext(services, configuration);
         AddRepositories(services);
+        AddToken(services, configuration);
 
         services.AddScoped<IPassWordEncripter, Security.Cryptography.BCrypt>();
+    }
+    private static void AddToken(this IServiceCollection services, IConfiguration configuration)
+    {
+        var expirationsTimeMinutes = configuration.GetValue<uint>("Settings:Jwt:ExpiresMinutes");
+        var signingKey = configuration.GetValue<string>("Settings:Jwt:SigningKey");
+        services.AddScoped<IAccessTokenGenerator>(config => new JwtTokenGenerator(expirationTimeMinutes: expirationsTimeMinutes, signingKey: signingKey!));
+
     }
 
     private static void AddRepositories(IServiceCollection services)
