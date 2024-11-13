4d79eccc44b7ef63e1e44191ca5f72ece10b5810
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2024-11-13 11:00:02 -0300

    fix: validando o usuario nos metodos delete, getall, getById, update da despesas

--- src/CashFlow.Application/UseCases/Expenses/GetAll/GetAllExpensesUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/GetAll/GetAllExpensesUseCase.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Domain.Services.LoggedUser;
 
 namespace CashFlow.Application.UseCases.Expenses.GetAll;
 
@@ -8,14 +9,21 @@ public class GetAllExpensesUseCase : IGetAllExpensesUseCase
 {
     private readonly IExpensesReadOnlyRepository _repository;
     private readonly IMapper _mapper;
-    public GetAllExpensesUseCase(IExpensesReadOnlyRepository repository, IMapper mapper)
+    private readonly ILoggedUser _loggedUser;
+    public GetAllExpensesUseCase(
+        IExpensesReadOnlyRepository repository, 
+        IMapper mapper,
+        ILoggedUser loggedUser)
     {
         _repository = repository;
         _mapper = mapper;
+        _loggedUser = loggedUser;
     }
     public async Task<ResponseExpensesJson> Execute()
     {
-        var result = await _repository.GetAll();
+        var loggedUser = await _loggedUser.Get();
+
+        var result = await _repository.GetAll(loggedUser);
 
         return new ResponseExpensesJson
         {
--- src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
+++ src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
@@ -16,33 +16,25 @@ internal class ExpensesRepository : IExpensesReadOnlyRepository, IExpensesWriteO
         await _dbContext.Expenses.AddAsync(expense);
     }
 
-    public async Task<List<Expense>> GetAll()
+    public async Task<List<Expense>> GetAll(User user)
     {
-        return await _dbContext.Expenses.AsNoTracking().ToListAsync();
+        return await _dbContext.Expenses.AsNoTracking().Where(e => e.UserId.Equals(user.Id)).ToListAsync();
     }
 
-    async Task<Expense?> IExpensesReadOnlyRepository.GetById(long id)
+    async Task<Expense?> IExpensesReadOnlyRepository.GetById(User user, long id)
     {
-       return await _dbContext.Expenses.AsNoTracking().FirstOrDefaultAsync(e => e.Id == id);
+       return await _dbContext.Expenses.AsNoTracking().FirstOrDefaultAsync(e => e.Id.Equals(id) && e.UserId.Equals(user.Id));
     }
 
-    async Task<Expense?> IExpensesUpdateOnlyRepository.GetById(long id)
+    async Task<Expense?> IExpensesUpdateOnlyRepository.GetById(User user, long id)
     {
-        return await _dbContext.Expenses.FirstOrDefaultAsync(e => e.Id == id);
+        return await _dbContext.Expenses.FirstOrDefaultAsync(e => e.Id.Equals(id) && e.UserId.Equals(user.Id));
     }
 
-    public async Task<bool> Delete(long id)
+    public async Task Delete(long id)
     {
-        var result = await _dbContext.Expenses.FirstOrDefaultAsync(e => e.Id == id);
-
-        if(result is null)
-        {
-            return false;
-        }
-
-        _dbContext.Expenses.Remove(result);
-
-        return true;
+        var result = await _dbContext.Expenses.FindAsync(id);
+        _dbContext.Expenses.Remove(result!);
     }
 
     public void Update(Expense expense)
